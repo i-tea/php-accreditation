045846aa3a31a4969d1e3d4e411746cd31d86c75
i-tea <i-tea@email.com>
2017-04-20 00:06:33 +0200

    the basic php files, images and css

---
+++ about.php
@@ -0,0 +1,10 @@
+<?php include 'header.php'; ?>
+
+
+	<div class="content container">
+		<h1 class="shadow">About us</h1>
+		<h2 class="shadow">We are better than U</h2>
+	</div>
+
+
+<?php include 'footer.php'; ?>
---
+++ header.php
@@ -0,0 +1,41 @@
+<?php 
+	
+	$page_name = basename($_SERVER['SCRIPT_NAME'], '.php');
+	if ( $page_name == 'index' ) $page_name = 'home';
+
+?>
+<!DOCTYPE html>
+<html>
+<head>	
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	
+	<title><?php echo ucfirst($page_name) ?> / Kickass Website</title>
+	
+	<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Montserrat:400,700">
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body class="<?php echo $page_name ?>">
+
+	<header class="site-header">
+		<nav class="container">
+			<ul class="menu">
+			<?php
+				if ($page_name =='home') echo '<li><strong>Home</strong></li>';
+				else echo '<li><a href="index.php">Home</a></li>';
+
+				if ($page_name =='gallery') echo '<li><strong>Gallery</strong></li>';
+				else echo '<li><a href="gallery.php">Gallery</a></li>';
+
+				if ($page_name =='blog') echo '<li><strong>Blog</strong></li>';
+				else echo '<li><a href="blog.php">Blog</a></li>';
+				
+				if ($page_name =='contact') echo '<li><strong>Contact</strong></li>';
+				else echo '<li><a href="contact.php">Contact</a></li>';
+				
+			?>
+			</ul>
+		</nav>
+	</header>
+
+	<main>
